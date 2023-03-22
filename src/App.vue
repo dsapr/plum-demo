9fbc5f250755cbffbefebc88feb2787f67131aaf
chenyi wang <785722840@qq.com>
2023-03-22 11:27:07 +0800

    feat: init plum

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,76 @@ interface Branch {
 function init() {
   ctx.strokeStyle = '#000'
 
-  const line = {
+  step({
     start: { x: WIDTH / 2, y: HEIGHT },
-    length: 100,
+    length: 20,
     theta: -Math.PI / 2,
-  }
-  drawBranch(line)
+  })
+
+  // const branch = {
+  //   start: { x: WIDTH / 2, y: HEIGHT },
+  //   length: 100,
+  //   theta: -Math.PI / 2,
+  // }
+  // const end = getEndPoint(branch)
+  // drawBranch(branch)
+
+  // const leftBranch = {
+  //   start: end,
+  //   length: 100,
+  //   theta: branch.theta - 0.1,
+  // }
+  // drawBranch(leftBranch)
+
+  // const rightBranch = {
+  //   start: end,
+  //   length: 100,
+  //   theta: branch.theta + 0.1,
+  // }
+  // drawBranch(rightBranch)
 }
+
+const pendingTasks: Function[] = []
+
+function step(b: Branch, depth = 0) {
+  const end = getEndPoint(b)
+  drawBranch(b)
+
+  if (depth < 2 || Math.random() < 0.5) {
+    pendingTasks.push(() => step ({
+      start: end,
+      length: b.length + (Math.random() * 10 - 5),
+      theta: b.theta - 0.3 * Math.random(),
+    }, depth + 1))
+  }
+
+  if (depth < 2 || Math.random() < 0.5) {
+    pendingTasks.push(() => step ({
+      start: end,
+      length: b.length + (Math.random() * 10 - 5),
+      theta: b.theta + 0.3 * Math.random(),
+    }, depth + 1))
+  }
+}
+
+function frame() {
+  const tasks = [...pendingTasks]
+  pendingTasks.length = 0
+  tasks.forEach(fn => fn())
+}
+
+let framesCount = 0
+function startFrame() {
+  requestAnimationFrame(() => {
+    framesCount += 1
+    // 3 帧画一次
+    if (framesCount % 3 === 0)
+      frame()
+    startFrame()
+  })
+}
+
+startFrame()
 
 function lineTo(p1: Point, p2: Point) {
   ctx.beginPath()
@@ -34,13 +97,16 @@ function lineTo(p1: Point, p2: Point) {
   ctx.stroke()
 }
 
-function drawBranch(l: Branch) {
-  const { start, length, theta } = l
-  const end = {
-    x: start.x + length * Math.cos(theta),
-    y: start.y + length * Math.sin(theta),
+function getEndPoint(b: Branch) {
+  return {
+    x: b.start.x + b.length * Math.cos(b.theta),
+    y: b.start.y + b.length * Math.sin(b.theta),
   }
-  lineTo(start, end)
+}
+
+function drawBranch(l: Branch) {
+  const end = getEndPoint(l)
+  lineTo(l.start, end)
 }
 
 onMounted(() => {
